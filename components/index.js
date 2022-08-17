d036393ae65c07aa271d20c9e08f253bb5fbc0c1
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-08-17 17:38:03 +0800

    added Category, Header, Layout, PostCard, PostWdiget components

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Categories from './Categories';
+import PostCard from './PostCard';
+import PostWidget from './PostWidget';
+import Header from './Header';
+import Layout from './Layout';
+
+export {
+  Categories, PostCard, PostWidget, Header, Layout
+}
